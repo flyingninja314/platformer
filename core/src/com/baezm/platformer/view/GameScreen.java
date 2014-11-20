d42e01823a8dd1acbad3b76769792a529a85b0de
flyingninja314 <drw.ninja@gmail.com>
2014-11-20 13:32:26 -0800

    20 November 2014

--- core/src/com/baezm/platformer/view/GameScreen.java
+++ core/src/com/baezm/platformer/view/GameScreen.java
@@ -1,5 +1,6 @@
 package com.baezm.platformer.view;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -16,12 +17,19 @@ public class GameScreen implements Screen {
 //        gets the map
         renderer = new OrthogonalTiledMapRenderer(map, 1/70f);
 //        tells program the size of a tile ===> f stands for floating decimal number
-        camera = new OrthographicCamera(14f, 14f);
+
+        float width = Gdx.graphics.getWidth();
+        float height = Gdx.graphics.getHeight();
+//        gets the height and width of the window so that the map isn't warped
+
+        camera = new OrthographicCamera(14f, 14f * (height / width));
 //        OrthographicCamera(14f, 14f) says that the camera sees 14 tiles up and 14 tiles across ===> this seats the frame
+//        fixes the warping of the map
         camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
 //          camera view is like a graph, so without setting camera position, the map appears in the 1st quadrant of a 4 quadrant graph (       |  x    ) and this line fixes it so that the map is on the whole screen
 //                                                                                                                                     (-------|-------)
 //                                                                                                                                     (       |       )
+
     }
 
     @Override
